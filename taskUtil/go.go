da944056979baef423f5a3d920a3e5972e4bdcc5
Christopher Jackson <hello@cj-jackson.com>
2020-04-19 16:58:34 +0100

    Remove something that is not used.

--- taskUtil/go.go
+++ taskUtil/go.go
@@ -2,9 +2,6 @@ package taskUtil
 
 import (
 	"os"
-	"os/exec"
-
-	"github.com/cjtoolkit/taskforce"
 )
 
 func goCommand() string {
@@ -14,16 +11,4 @@ func goCommand() string {
 	}
 
 	return "go"
-}
-
-func GoBuild(tf *taskforce.TaskForce) func(output, src string) {
-	return func(output, src string) {
-		cmd := exec.Command(goCommand(), "build", "-v", "-o", output, src)
-		cmd.Env = append(os.Environ(), "GOOS=linux", "GOARCH=amd64")
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-
-		tf.CheckError(cmd.Run())
-	}
 }
