7314cfb4dca1b5019e5625c3ae8bcd92459cc4be
Christopher Jackson <hello@cj-jackson.com>
2020-04-20 07:40:21 +0100

    Clean up generators

--- prGen/internal/generate.go
+++ prGen/internal/generate.go
@@ -1,4 +1,3 @@
-//go:generate gobox tools/embedder internal generated_const.go resources/*.html
-//go:generate gobox tools/embedder internal generated_txt_const.go resources/*.txt
+//go:generate gobox tools/embedder internal generated_const.go resources/*
 
 package internal
